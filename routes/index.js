37bd19d5be17548cad72ceea07b2494505e629db
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-08-30 17:58:46 +0700

    First Initial

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login-signup', (req, res, next) => {
+  res.render('login-signup', { title: 'Express' });
 });
 
 module.exports = router;
